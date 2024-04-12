a603daf8a6456c938a9b1e645833fabdbe84f436
halanmcctb <halanmca123@gmail.com>
2024-04-11 23:07:26 -0700

    Created person properties

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+// alert("Hello World");
+
+let name = "Jhon Doe";
+let age = 26;
+let isAlive = true;
+const father = "Richard Doe";
+const mother = "Janie Doe";
+
+console.log(`Person name is: ${name}`);
+console.log(`Person age is: ${age}`);
+console.log(`Person is alive: ${isAlive ? "Yes" : "No"}`);
+console.log(`Person father is: ${father}`);
+console.log(`Person mother is: ${mother}`);
