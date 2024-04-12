3aa17a31c1dd8e03ced569607aa6ce42e9cb60ef
halanmcctb <halanmca123@gmail.com>
2024-04-11 23:14:53 -0700

    Created a to do list

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // alert("Hello World");
 
-let name = "Jhon Doe";
-let age = 26;
-let isAlive = true;
-const father = "Richard Doe";
-const mother = "Janie Doe";
+// let name = "Jhon Doe";
+// let age = 26;
+// let isAlive = true;
+// const father = "Richard Doe";
+// const mother = "Janie Doe";
 
-console.log(`Person name is: ${name}`);
-console.log(`Person age is: ${age}`);
-console.log(`Person is alive: ${isAlive ? "Yes" : "No"}`);
-console.log(`Person father is: ${father}`);
-console.log(`Person mother is: ${mother}`);
+// console.log(`Person name is: ${name}`);
+// console.log(`Person age is: ${age}`);
+// console.log(`Person is alive: ${isAlive ? "Yes" : "No"}`);
+// console.log(`Person father is: ${father}`);
+// console.log(`Person mother is: ${mother}`);
+
+let toDo = [
+  "Attempt to make breakfast without burning down the kitchen",
+  "Have a heartfelt conversation with my pet goldfish about the meaning of life",
+  "Try to convince myself that today will be the day I'll finally become a morning person",
+  "Attempt to stay awake during a meeting that could have been an email",
+];
